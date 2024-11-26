bea683648d46ae83d62b2185045ec96811bfd17e
Hayden <haydendanielmckenzie@gmail.com>
2024-11-26 13:13:27 +1100

    Added Functionality to Social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,44 @@
-import { pageLinks } from '../data'
+import { socialLinks, pageLinks } from '../data';
 
 // Under pageLinks -> Added away before the tutorial
 
 const Footer = () => {
-    return (
-        <footer className="section footer">
-            <ul className="footer-links">
-                {pageLinks.map((link) => {
-                    return (
-                        <li key={link.id}>
-                            <a href={link.href} className="footer-link">
-                                {link.text}
-                            </a>
-                        </li>
-                    )
-                })}
-            </ul>
+  return (
+    <footer className="section footer">
+      <ul className="footer-links">
+        {pageLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.href} className="footer-link">
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
 
-            <ul className="footer-icons">
-                <li>
-                    <a
-                        href="https://www.twitter.com"
-                        target="_blank"
-                        className="footer-icon"
-                    >
-                        <i className="fab fa-facebook"></i>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://www.twitter.com"
-                        target="_blank"
-                        className="footer-icon"
-                    >
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="https://www.twitter.com"
-                        target="_blank"
-                        className="footer-icon"
-                    >
-                        <i className="fab fa-squarespace"></i>
-                    </a>
-                </li>
-            </ul>
-            <p className="copyright">
-                copyright &copy; Backroads travel tours company
-                <span id="date"></span> all rights reserved
-            </p>
-        </footer>
-    )
-}
-export default Footer
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          return (
+            <li key={link.id}>
+              <a
+                href={link.href}
+                target="_blank"
+                rel="noreferrer"
+                className="footer-icon"
+              >
+                <i className={link.icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date"></span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,41 @@
-import logo from '../images/logo.svg'
-import { pageLinks } from '../data'
+import logo from '../images/logo.svg';
+import { pageLinks, socialLinks } from '../data';
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="nav-center">
-                <div className="nav-header">
-                    <img src={logo} className="nav-logo" alt="backroads" />
-                    <button
-                        type="button"
-                        className="nav-toggle"
-                        id="nav-toggle"
-                    >
-                        <i className="fas fa-bars"></i>
-                    </button>
-                </div>
-                <ul className="nav-links" id="nav-links">
-                    {pageLinks.map((link) => {
-                        return (
-                            <li key={link.id}>
-                                <a href={link.href} className="nav-link">
-                                    {link.text}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
 
-                <ul className="nav-icons">
-                    <li>
-                        <a
-                            href="https://www.twitter.com"
-                            target="_blank"
-                            className="nav-icon"
-                        >
-                            <i className="fab fa-facebook"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.twitter.com"
-                            target="_blank"
-                            className="nav-icon"
-                        >
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://www.twitter.com"
-                            target="_blank"
-                            className="nav-icon"
-                        >
-                            <i className="fab fa-squarespace"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
-export default Navbar
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-icon">
+                  <i className={link.icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
