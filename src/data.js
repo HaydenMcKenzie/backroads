57c1d0dd91d35d0968fdbcce5bf0c5159c8ad0da
Hayden <haydendanielmckenzie@gmail.com>
2024-11-28 12:59:05 +1100

    Added Stuff

--- src/data.js
+++ src/data.js
@@ -7,17 +7,26 @@ export const pageLinks = [
 export const socialLinks = [
   {
     id: 1,
-    href: 'https://www.twitter.com"',
+    href: 'https://www.twitter.com',
     icon: 'fab fa-facebook',
   },
   {
     id: 2,
-    href: 'https://www.twitter.com"',
+    href: 'https://www.twitter.com',
     icon: 'fab fa-twitter',
   },
   {
     id: 3,
-    href: 'https://www.twitter.com"',
+    href: 'https://www.twitter.com',
     icon: 'fab fa-squarespace',
+  },
+];
+
+export const serviceList = [
+  {
+    id: 1,
+    title: 'saving money',
+    icon: 'fas fa-wallet fa-fw',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sapiente incidunt accusamus quae veniam, possimus, mollitia ad laborum nesciunt alias delectus magni assumenda. Voluptatum vel quasi sed est temporibus cum.',
   },
 ];
